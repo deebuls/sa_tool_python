630a96a7a080392871792e35ceb41709974b8e64
deebuls <deebuls@gmail.com>
2015-05-26 15:48:21 +0200

    Adding tool and example usage

---
+++ examples_sa_tool.py
@@ -0,0 +1,78 @@
+import sa_tool
+import networkx as nx
+
+if __name__ == '__main__' :
+    if(nx.__version__ != '2.0.dev_20150521003926'):
+        print "networkx library error"
+        print "ERROR : max match algorithm is implemented in the 2.0 dev branch. Please install 2.0 dev branch of networkx"
+        exit()
+
+    B = nx.Graph(name="Example 5.17")
+    B.add_nodes_from(['x1', 'x2', 'x3', 'x4', 'x5'], bipartite=0, color='r') # Add the node attribute "bipartite"
+    B.add_nodes_from(['c1','c2','c3', 'c4', 'c5', 'c6'], bipartite=1, color='b')
+    B.add_edges_from([('x1','c1'),('x1','c2'),('x1','c4') ])
+    B.add_edges_from([('x2','c2'),('x2','c5')])
+    B.add_edges_from([('x3','c2'),('x3','c3')])
+    B.add_edges_from([('x4','c3'),('x4','c4'),('x4','c6') ])
+    B.add_edges_from([('x5','c5'),('x5','c6')])
+    
+    sa1 = sa_tool.SATool(B)
+    sa1.visualize_bipartite()
+    sa1.calculate_maximum_matching()
+    sa1.visualize_bipartite(with_matching=True)
+    sa1.calculate_orientation()
+    sa1.visualize_bipartite(with_orientation=True)
+
+    
+    B = nx.Graph(name="Example 5.3 Linear System")
+    B.add_nodes_from(['x1', 'x2', 'u', 'x1_dot', 'x1_dot'], bipartite=0, color='r') # Add the node attribute "bipartite"
+    B.add_nodes_from(['c1','c2','c3', 'c4'], bipartite=1, color='b')
+    B.add_edges_from([('u','c4')])
+    B.add_edges_from([('x1','c1'),('x1','c4') ])
+    B.add_edges_from([('x2','c2'),('x2','c3'),('x2','c4') ])
+    B.add_edges_from([('x1_dot','c1'),('x1_dot','c2')])
+    B.add_edges_from([('x2_dot','c3'),('x2_dot','c4')])
+    
+    sa1 = sa_tool.SATool(B)
+    sa1.visualize_bipartite()
+    sa1.calculate_maximum_matching()
+    sa1.visualize_bipartite(with_matching=True)
+    sa1.calculate_orientation()
+    sa1.visualize_bipartite(with_orientation=True)
+
+
+    
+    B = nx.Graph(name=" Tank System")
+    B.add_nodes_from(['h', 'h_dot', 'q_i', 'q_o'], bipartite=0, color='r', status='unknown') # Add the node attribute "bipartite"
+    B.add_nodes_from(['u', 'y'], bipartite=0, color='r', status='known') # Add the node attribute "bipartite"
+    B.add_nodes_from(['c1','c2','c3', 'c4', 'c5', 'c6'], bipartite=1, color='b')
+    B.add_edges_from([('u','c2'),('u','c5')])
+    B.add_edges_from([('h_dot','c1'),('h_dot','c6') ])
+    B.add_edges_from([('h','c6'),('h','c3'),('h','c4') ])
+    B.add_edges_from([('q_i','c1'),('q_i','c2')])
+    B.add_edges_from([('q_o','c3'),('q_o','c1')])
+    B.add_edges_from([('y','c5'),('y','c4')])
+    
+    sa1 = sa_tool.SATool(B)
+    sa1.visualize_bipartite()
+    sa1.calculate_maximum_matching()
+    sa1.visualize_bipartite(with_matching=True)
+    sa1.calculate_orientation()
+    sa1.visualize_bipartite(with_orientation=True)
+
+    
+    B = nx.Graph(name=" Tank System")
+    B.add_nodes_from(['h', 'h_dot', 'q_i', 'q_o'], bipartite=0, color='r', status='unknown') # Add the node attribute "bipartite"
+    B.add_nodes_from(['c1','c2','c3', 'c4', 'c5', 'c6'], bipartite=1, color='b')
+    B.add_edges_from([('h_dot','c1'),('h_dot','c6') ])
+    B.add_edges_from([('h','c3'),('h','c4') ])
+    B.add_edges_from([('q_i','c1'),('q_i','c2')])
+    B.add_edges_from([('q_o','c3'),('q_o','c1')])
+    
+    sa1 = sa_tool.SATool(B)
+    sa1.visualize_bipartite()
+    sa1.calculate_maximum_matching()
+    sa1.visualize_bipartite(with_matching=True)
+    sa1.calculate_orientation()
+    sa1.visualize_bipartite(with_orientation=True)
+
---
+++ sa_tool.py
@@ -0,0 +1,126 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import sys
+class SATool:
+    """"
+    Class for doing structural analysis using graphs.
+    The class takes the graph produced by networkx and
+    provides manipulation on the data
+    """
+
+    def __init__(self, graph):
+        if (False == nx.is_bipartite(graph)):
+            print "[ERROR]Not a bipartite graph provided."
+            sys.exit()
+
+        self.G = graph
+        self.D = graph.to_directed()
+        self.variables, self.constraints = nx.bipartite.sets(self.G)
+        Y = list(self.variables)
+        if (self.G.node[Y[0]]['bipartite'] == 1):
+            X = self.constraints
+            self.constraints = self.variables
+            self.variables = X
+
+    def calculate_maximum_matching(self):
+        self.max_match_dict = nx.algorithms.bipartite.maximum_matching(self.G)
+        self.max_match_list = list(self.max_match_dict.items())
+
+    def calculate_orientation(self):
+        '''
+        bipartite =1 means the node is a constraint node
+        constructing the orientation graph .
+        Orientation :
+            For matched constraints :
+                from non-matched variable to constraint
+                from constraint to matched variable
+            for non matched constraints :
+                from all varaibles to constraints
+                
+        Algo :
+            The max matching list contains edge in both direction 
+            i.e. from constraint to variables and variables to constraints
+            loop on the matching list
+            check if the edge startd from a constraint node
+            if yes 
+                add the edge to the orientation list
+                find connected nodes of the constraint node
+                add edge from connected node to the constraint node
+        '''
+        self.calculate_maximum_matching()
+        #Finding unmatched constratints
+        self.unmatched_constraints =  self.constraints - set(self.max_match_dict.keys())
+
+        self.orientation_graph = []
+
+        for e in self.max_match_list:
+            #check if the edge starts from a constraint node
+            if (self.D.node[e[0]]['bipartite'] == 1):
+                self.orientation_graph.append(e)
+                for s in self.D.successors(e[0]):
+                    if (s != e[1]):
+                        self.orientation_graph.append((s,e[0]))
+                        
+        #for non matched constraints
+        for constraint in self.unmatched_constraints:
+            for variable in self.D.successors(constraint):
+                self.orientation_graph.append((variable,constraint))
+
+    def visualize_bipartite(self, with_matching=False,with_orientation=False):
+        pos = dict()
+        pos.update( (n, (1, i)) for i, n in enumerate(self.constraints) ) # put nodes from self.variables at x=1
+        pos.update( (n, (2, i)) for i, n in enumerate(self.variables) ) # put nodes from self.constraints at x=2
+    
+        fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(8,8))
+        # nodes
+        nx.draw_networkx_nodes(self.G,pos,ax=ax1,
+                            nodelist=self.variables,
+                            node_color='g',
+                            node_size=500,
+                            alpha=0.8)
+        nx.draw_networkx_nodes(self.G,pos,ax=ax1,
+                            nodelist=self.constraints,
+                            node_color='y',
+                            node_size=500,
+                        alpha=0.8)
+        # edges
+        nx.draw_networkx_edges(self.G,pos,width=1.0,alpha=0.5)
+        nx.draw_networkx_labels(self.G,pos)
+
+        fig.suptitle(self.G.name, fontsize=14, fontweight='bold')
+        plt.savefig("./images/" + self.G.name + ".png")
+        if(with_matching ):
+            ax1.set_title("Max Matching")
+            self.calculate_maximum_matching()
+            nx.draw_networkx_edges(self.G,pos,ax=ax1,
+                                edgelist=self.max_match_list,
+                                width=8,alpha=0.3,edge_color='b')
+            plt.savefig("./images/" + self.G.name+"_MaxMatching" + ".png")
+        if(with_orientation):
+            ax1.set_title("With Orientation")
+            self.calculate_maximum_matching()
+            self.calculate_maximum_matching()
+            nx.draw_networkx_edges(self.G,pos,ax=ax1,
+                                edgelist=self.max_match_list,
+                                width=5,alpha=0.3,edge_color='b')
+            self.calculate_orientation()
+            # nodes
+            nx.draw_networkx_nodes(self.G,pos,ax=ax1,
+                                nodelist=self.unmatched_constraints,
+                                node_color='r',
+                                node_size=500,
+                                alpha=0.8)
+            nx.draw_networkx_edges(self.D,pos,ax=ax1,
+                       edgelist=self.orientation_graph, arrows=True,
+                       width=1,alpha=0.5)
+            plt.savefig("./images/" + self.G.name+"_WithOrientation" + ".png")
+        '''
+        red_patch = mpatches.Patch(color='red', label='Unmatched Constraint')
+        yellow_patch = mpatches.Patch(color='yellow', label='Variables')
+        green_patch = mpatches.Patch(color='green', label='Constraints')
+        plt.legend(handles=[red_patch, yellow_patch, green_patch]) 
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+        '''
+        fig.suptitle(self.G.name, fontsize=14, fontweight='bold')
+        plt.show()
